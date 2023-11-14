77c784cafc18354a0592217678131545db8c4abb
OliverBoesen <145548550+OliverBoesen@users.noreply.github.com>
2023-11-14 12:13:47 +0100

    Delete kontakt directory

--- kontakt/java.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('menu-icon').addEventListener('click', function() {
-        const mobilMenu = document.getElementById('menu-list');
-        mobilMenu.classList.toggle('show');
-    });
-});
